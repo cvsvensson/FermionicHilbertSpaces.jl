9d2bd7afd84bcc4ffed13be2d11117c963d01a43
Documenter.jl <documenter@juliadocs.github.io>
2025-06-08 07:53:58 +0000

    build based on eeebe2f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FermionicHilbertSpaces","page":"Home","title":"FermionicHilbertSpaces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FermionicHilbertSpaces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
