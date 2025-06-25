5092b8f04d32a8d65368c5aa0f6ba3a9a498f3e8
Viktor Svensson <svensson.viktor.91@gmail.com>
2025-06-25 15:55:58 +0200

    Revert "better type inference in matrix_representation"
    This reverts commit fed3360b0a6250c68a2ef222daade139ceb7b26f.

--- src/symbolics/muladd.jl
+++ src/symbolics/muladd.jl
@@ -250,7 +250,7 @@ function matrix_representation(op::FermionAdd{C}, jw, outstates, instates) where
     for op in fermionterms(op)
         sparsetuple!((outinds, ininds, amps), op, jw, outstates, instates; fock_to_outind=fock_to_outind)
     end
-    return sparse(outinds, ininds, [amps...], length(outstates), length(instates))
+    return sparse(outinds, ininds, identity.(amps), length(outstates), length(instates))
 end
 sparsetuple!((outinds, ininds, amps), op::AbstractFermionSym, jw, outstates, instates; kwargs...) = sparsetuple!((outinds, ininds, amps), FermionMul(1, [op]), jw, outstates, instates; kwargs...)
 
