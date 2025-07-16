7753a2e0b236d9e72097a7efd303dceba94f7c25
Viktor Svensson <svensson.viktor.91@gmail.com>
2025-07-16 14:40:13 +0200

    Run literate to create documentation during the docs build (#34)
    * expand kitaev chain tutorial
    
    * simplify extend for DirectSum
    
    * convert example to tutorial
    
    * fix typo
    
    * plot styling
    
    * fix typo
    
    * try to include literate in docs build
    
    * fixes for literate with docs
    
    * fix directory path
    
    * minor fix to kitaev_chain example
    
    * improve kitaev_chain example
    
    ---------
    
    Co-authored-by: William Samuelson <william.samuelson@gmail.com>

--- examples/kitaev_chain.jl
+++ examples/kitaev_chain.jl
@@ -13,12 +13,11 @@ N = 12
 H = hilbert_space(1:N, ParityConservation())
 
 # Symbolic fermions can be defined using the `@fermions` macro,
-@fermions fsym
+@fermions f
 
 # Let's define the interacting Kitaev chain Hamiltonian.
 # It is a function of the fermions `f` and parameters `N`, `μ`, `t`, `Δ`, and `U`,
 # representing the number of sites, chemical potential, hopping amplitude, pairing amplitude, and interaction strength, respectively.
-# Here, f can be either symbolic (`fsym`) or a matrix representation (`fmat`).
 # Note the use of the Hermitian conjugate `hc`, which simplifies the expression for the Hamiltonian.
 kitaev_chain(f, N, μ, t, Δ, U) = sum(t * f[i]' * f[i+1] + hc for i in 1:N-1) +
                                  sum(Δ * f[i] * f[i+1] + hc for i in 1:N-1) +
@@ -34,9 +33,10 @@ t = 1.0
 μ[2:N-1] .= -U # bulk chemical potential
 
 # We can now construct the Hamiltonian using symbolic fermions for a symbolic representation
-hsym = kitaev_chain(fsym, N, μ, t, Δ, U)
+hsym = kitaev_chain(f, N, μ, t, Δ, U)
 # To convert the symbolic Hamiltonian to a matrix representation, we can use the `matrix_representation` function.
 matrix_representation(hsym, H)
+
 
 # Now, let's diagonalize the system.
 # Since parity is conserved, we can work in the even and odd parity sectors separately.
@@ -65,7 +65,7 @@ e = extend(evenvec, Heven => Hsum) # even ground state in the full Hilbert space
 # Then, we can construct the ground state Majorana operators as
 # γ = o * e' + hc
 # γ̃ = 1im * o * e' + hc
-# but that takes a lot of memory for large systems
+# but that takes a lot of memory for large systems. Let's make a quick struct representing a rank-1 matrix and use that instead.
 struct Rank1Matrix{T} <: AbstractMatrix{T}
     vec1::Vector{T}
     vec2::Vector{T}
