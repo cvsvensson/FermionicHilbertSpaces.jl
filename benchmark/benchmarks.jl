29c9da1ef955d95c9432ae2714d12c0e257e651c
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2025-08-15 08:23:27 +0200

    more benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,7 +1,9 @@
 using BenchmarkTools
 using FermionicHilbertSpaces
 using SparseArrays
+using Random
 const SUITE = BenchmarkGroup()
+seed!(1)
 
 N = 12
 H = hilbert_space(1:N, ParityConservation())
@@ -11,10 +13,22 @@ Hsub = hilbert_space(1:div(N, 4), ParityConservation())
 m = sprand(ComplexF64, size(H)..., 1 / 2^N)
 
 SUITE["hilbert_space"] = @benchmarkable hilbert_space($(1:N), $ParityConservation())
-SUITE["symbolic_sum"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:N)
-SUITE["matrix_representation"] = @benchmarkable matrix_representation($op, $H)
-SUITE["partial_trace"] = @benchmarkable partial_trace($m, $(H => Hsub))
+SUITE["symbolic_sum"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:100)
+SUITE["symbolic_sum_square"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:50)^2
 
+labels = shuffle(1:10)
+SUITE["symbolic_deep_product"] = @benchmarkable prod(f[l] for l in labels) * prod(f[l]' for l in labels)
+SUITE["matrix_representation"] = @benchmarkable matrix_representation($op, $H)
+
+Hsp = single_particle_hilbert_space(1:1000)
+opsp = sum(rand() * f[n]' * f[n] for n in 1:1000) + sum(rand(ComplexF64) * f[n]' * f[n+1] + hc for n in 1:999)
+SUITE["matrix_representation_free_fermion"] = @benchmarkable matrix_representation($opsp, $Hsp)
+
+opsp_bdg = opsp + sum(rand(ComplexF64) * f[n]' * f[n+1]' + hc for n in 1:999)
+Hbdg = bdg_hilbert_space(1:1000)
+SUITE["matrix_representation_bdg"] = @benchmarkable matrix_representation($opsp_bdg, $Hbdg)
+
+SUITE["partial_trace"] = @benchmarkable partial_trace($m, $(H => Hsub))
 msub = rand(ComplexF64, size(Hsub))
 SUITE["embed"] = @benchmarkable FermionicHilbertSpaces.embedding($msub, $(Hsub => H))
 
