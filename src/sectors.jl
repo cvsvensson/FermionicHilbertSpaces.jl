42d81e367e52208fb71779404ac09f73fd17aa8f
William Samuelson <william.samuelson@gmail.com>
2025-09-01 10:27:40 +0200

    indices from qn

--- src/sectors.jl
+++ src/sectors.jl
@@ -1,6 +1,9 @@
 sector(qn, H::SymmetricFockHilbertSpace) = hilbert_space(keys(H), H.symmetry.qntofockstates[qn])
 
 indices(Hsub::AbstractHilbertSpace, H::AbstractHilbertSpace) = map(Base.Fix2(state_index, H), basisstates(Hsub))
+function indices(qn::QN, H::SymmetricFockHilbertSpace{L,FockSymmetry{IF,FI,QN,I,QNfunc}}) where {QN,L,IF,FI,I,QNfunc}
+    map(Base.Fix2(state_index, H), H.symmetry.qntofockstates[qn])
+end
 
 @testitem "Sector" begin
     import FermionicHilbertSpaces: sector, indices
@@ -10,5 +13,6 @@ indices(Hsub::AbstractHilbertSpace, H::AbstractHilbertSpace) = map(Base.Fix2(sta
         Hn = hilbert_space(1:N, NumberConservation(n))
         @test basisstates(Hn) == basisstates(sector(n, H))
         @test basisstates(Hn) == basisstates(H)[indices(Hn, H)]
+        @test basisstates(Hn) == basisstates(H)[indices(n, H)]
     end
 end
