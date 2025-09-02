05f4f71a1378e944bd8ae42b7e174869f007e068
William Samuelson <william.samuelson@gmail.com>
2025-09-02 09:53:47 +0200

    quantumnumbers and sectors

--- src/sectors.jl
+++ src/sectors.jl
@@ -1,4 +1,6 @@
 sector(qn, H::SymmetricFockHilbertSpace) = hilbert_space(keys(H), H.symmetry.qntofockstates[qn])
+quantumnumbers(H::SymmetricFockHilbertSpace) = collect(keys(H.symmetry.qntofockstates))
+sectors(H::SymmetricFockHilbertSpace) = map(Base.Fix2(sector, H), quantumnumbers(H))
 
 indices(Hsub::AbstractHilbertSpace, H::AbstractHilbertSpace) = map(Base.Fix2(state_index, H), basisstates(Hsub))
 function indices(qn::QN, H::SymmetricFockHilbertSpace{L,FockSymmetry{IF,FI,QN,I,QNfunc}}) where {QN,L,IF,FI,I,QNfunc}
@@ -6,11 +8,14 @@ function indices(qn::QN, H::SymmetricFockHilbertSpace{L,FockSymmetry{IF,FI,QN,I,
 end
 
 @testitem "Sector" begin
-    import FermionicHilbertSpaces: sector, indices
+    import FermionicHilbertSpaces: sector, sectors, indices, quantumnumbers
     N = 4
     H = hilbert_space(1:N, NumberConservation())
-    for n in 1:N
+    @test quantumnumbers(H) == collect(0:N)
+    Hns = sectors(H)
+    for n in quantumnumbers(H)
         Hn = hilbert_space(1:N, NumberConservation(n))
+        @test basisstates(Hn) == basisstates(Hns[n + 1]) # Hn ≠ Hns[N + 1] since Hn is a SymmetricFockHilbertSpace
         @test basisstates(Hn) == basisstates(sector(n, H))
         @test basisstates(Hn) == basisstates(H)[indices(Hn, H)]
         @test basisstates(Hn) == basisstates(H)[indices(n, H)]
