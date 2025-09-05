1691ca5db001d1b2b31eb7ce141452310dbe8e27
William Samuelson <78036889+williamesamuelson@users.noreply.github.com>
2025-09-05 10:06:16 +0200

    Symbolic partial trace (#67)
    * sketching symbolic partial trace for majoranas
    
    * partial trace for NCAdds and NCMuls
    
    * test against numerical partial trace
    
    * more general op in test
    
    Co-authored-by: Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
    
    * handle constants and provide init in sum
    
    ---------
    
    Co-authored-by: Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>

--- src/majorana_hilbert_space.jl
+++ src/majorana_hilbert_space.jl
@@ -36,6 +36,32 @@ function subregion(modes, H::MajoranaHilbertSpace)
     MajoranaHilbertSpace(majorana_position, subregion(pairs, H.parent))
 end
 partial_trace!(mout, m::AbstractMatrix, H::MajoranaHilbertSpace, Hsub::MajoranaHilbertSpace, phase_factors::Bool=true, complement::MajoranaHilbertSpace=simple_complementary_subsystem(H, Hsub)) = partial_trace!(mout, m, H.parent, Hsub.parent, phase_factors, complement.parent)
+function partial_trace(m::NCMul{C,S,F}, H::MajoranaHilbertSpace, Hsub::MajoranaHilbertSpace) where {C,S<:AbstractMajoranaSym,F}
+    sub_modes = Set(Iterators.flatten(modes(Hsub)))
+    for f in m.factors
+        if f.label ∉ sub_modes
+            return 0 * m
+        end
+    end
+    return m * dim(H) / dim(Hsub)
+end
+function partial_trace(m::NCAdd{C,NCMul{C2,S,F}}, H::MajoranaHilbertSpace, Hsub::MajoranaHilbertSpace) where {C,C2,S<:AbstractMajoranaSym,F}
+    return sum(partial_trace(term, H, Hsub) for term in NCterms(m); init=0*m) + m.coeff * dim(H) / dim(Hsub)
+end
+function partial_trace(m::NCAdd{C,NCMul{C2,S,F}}, Hs::Pair{<:MajoranaHilbertSpace,<:MajoranaHilbertSpace}) where {C,C2,S<:AbstractMajoranaSym,F}
+    return partial_trace(m, Hs...)
+end
+
+@testitem "Partial trace of symbolic Majoranas" begin
+    @majoranas y
+    H = majorana_hilbert_space(1:6)
+    Hsub = subregion(3:4, H)
+    op = 1 + 3y[1] + 2y[3] + 4y[1]*y[6] + 3y[4]*y[1] + y[3]*y[4] + y[1]*y[3]*y[4] + y[1]*y[2]*y[6]
+    op2 = 3 + 0y[1] # NCterms(op2) is empty
+    @test matrix_representation(partial_trace(op, H => Hsub), Hsub) == partial_trace(matrix_representation(op, H), H => Hsub)
+    @test matrix_representation(partial_trace(op2, H => Hsub), Hsub) == partial_trace(matrix_representation(op2, H), H => Hsub)
+end
+
 function simple_complementary_subsystem(H::MajoranaHilbertSpace, Hsub::MajoranaHilbertSpace)
     complement_labels = setdiff(keys(H.majoranaindices), keys(Hsub.majoranaindices))
     complement_fermionic_space = simple_complementary_subsystem(H.parent, Hsub.parent)
