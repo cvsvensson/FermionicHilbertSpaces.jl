74b4e53d8a3d6ac51aaa1a131c6314e473982958
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2025-11-20 15:54:12 +0100

    update benchmark script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -14,25 +14,35 @@ d = dim(H)
 m = sprand(ComplexF64, d, d, 1 / 2^N)
 
 SUITE["hilbert_space"] = @benchmarkable hilbert_space($(1:N), $ParityConservation())
-SUITE["symbolic_sum"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:100)
-SUITE["symbolic_sum_square"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:50)^2
+SUITE["symbolic"]["sum"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:100)
+SUITE["symbolic"]["sum_square"] = @benchmarkable sum(f[n]' * f[n] + hc for n in 1:50)^2
 
 labels = shuffle(1:10)
-SUITE["symbolic_deep_product"] = @benchmarkable prod(f[l] for l in labels) * prod(f[l]' for l in labels)
-SUITE["matrix_representation"] = @benchmarkable matrix_representation($op, $H)
+SUITE["symbolic"]["deep_product"] = @benchmarkable prod(f[l] for l in labels) * prod(f[l]' for l in labels)
+SUITE["matrix_representation"]["standard"] = @benchmarkable matrix_representation($op, $H)
 
 Hsp = single_particle_hilbert_space(1:1000)
 opsp = sum(rand() * f[n]' * f[n] for n in 1:1000) + sum(rand(ComplexF64) * f[n]' * f[n+1] + hc for n in 1:999)
-SUITE["matrix_representation_free_fermion"] = @benchmarkable matrix_representation($opsp, $Hsp)
+SUITE["matrix_representation"]["free_fermion"] = @benchmarkable matrix_representation($opsp, $Hsp)
 
 opsp_bdg = opsp + sum(rand(ComplexF64) * f[n]' * f[n+1]' + hc for n in 1:999)
 Hbdg = bdg_hilbert_space(1:1000)
-SUITE["matrix_representation_bdg"] = @benchmarkable matrix_representation($opsp_bdg, $Hbdg)
-
+SUITE["matrix_representation"]["bdg"] = @benchmarkable matrix_representation($opsp_bdg, $Hbdg)
 SUITE["partial_trace"] = @benchmarkable partial_trace($m, $(H => Hsub))
+# SUITE["partial_trace_map"] = @benchmarkable FermionicHilbertSpaces.partial_trace_map($(H => Hsub))
 d = dim(Hsub)
 msub = rand(ComplexF64, d, d)
 SUITE["embed"] = @benchmarkable embed($msub, $(Hsub => H))
+
+N = 60
+weights = [floor.(2sin.(1:N)), sign.((1:N) .- div(N, 2)), ones(N)]
+allowed_ones = [[0, 1], [-1, 0], [2]]
+SUITE["generate_states"]["int"] = @benchmarkable FermionicHilbertSpaces.generate_states($weights, $allowed_ones, $N)
+
+N = 64
+weights = [floor.(2sin.(1:N)), sign.((1:N) .- div(N, 2)), ones(N)]
+allowed_ones = [[0, 1], [-1, 0], [2]]
+SUITE["generate_states"]["big_int"] = @benchmarkable FermionicHilbertSpaces.generate_states($weights, $allowed_ones, $N)
 
 # using Symbolics
 # @variables x
