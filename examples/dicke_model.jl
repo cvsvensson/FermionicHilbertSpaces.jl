0bd69f1890bcfa310ea01bb646f72213412f8d27
William Samuelson <william.samuelson@gmail.com>
2026-03-26 17:15:44 +0100

    number and parity conservation for spins

--- examples/dicke_model.jl
+++ examples/dicke_model.jl
@@ -1,16 +1,16 @@
 using FermionicHilbertSpaces
-using FermionicHilbertSpaces: SpinSpace, TruncatedBosonicHilbertSpace
+
 N = 3
 @spins S 1:N
 @boson a
 
 ωc = 1.0
-ωs = 0.5
+ωz = 0.5
 λ = 0.1
-symham = ωc * a' * a + ωs * sum(S[k][:z] for k in 1:N) + 2λ / sqrt(N) * (a' + a) * sum(S[k][:x] for k in 1:N)
+symham = ωc * a' * a + ωz * sum(S[k][:z] for k in 1:N) + 2λ / sqrt(N) * (a' + a) * sum(S[k][:x] for k in 1:N)
 
-Hs = SpinSpace{1 // 2}.(S)
-Ha = TruncatedBosonicHilbertSpace(a, 10)
-H = tensor_product(Hs..., Ha)
+Hs = hilbert_space.(values(S), 1//2)
+Ha = hilbert_space(a, 10)
+H = tensor_product(Hs..., Ha, ParityConservation())
 ham = matrix_representation(symham, H)
 # One should use sectors with permutationally invariant states, but we need to add methods to constrain product spaces to do that
