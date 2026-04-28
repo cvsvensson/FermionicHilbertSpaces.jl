4f5113cdd83d18355632cbf549f6886080f5403b
William Samuelson <william.samuelson@gmail.com>
2026-04-28 13:32:10 +0200

    rewrite with direction, new file

--- src/FermionicHilbertSpaces.jl
+++ src/FermionicHilbertSpaces.jl
@@ -101,6 +101,7 @@ include("physics/fermions/symbolic_fermions.jl")
 include("physics/fermions/fermions.jl")
 include("physics/fermions/operators.jl")
 include("physics/fermions/majoranas.jl")
+include("physics/fermions/symbolic_conversion.jl")
 include("physics/fermions/fixednumberfock.jl")
 include("physics/fermions/bdg.jl")
 
