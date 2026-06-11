bbc9180d4909876d1dd2c6bfb01274dc028ff071
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2026-06-11 11:04:49 +0200

    add 'Base.getindex(s::SymbolicSpinBasis, ::Colon)' to get vector of paulis

--- src/physics/spin.jl
+++ src/physics/spin.jl
@@ -21,6 +21,7 @@ SymbolicSpinBasis(label::L, field::P=nothing, spin::J=nothing) where {L,P,J} = S
 Base.:(==)(a::SymbolicSpinBasis, b::SymbolicSpinBasis) = a.label == b.label && a.field == b.field && a.spin == b.spin && a.tags == b.tags
 Base.isless(a::SymbolicSpinBasis, b::SymbolicSpinBasis) = (a.field, a.label, a.tags) < (b.field, b.label, b.tags)
 Base.getindex(s::SymbolicSpinBasis, op) = SpinSym(op, s)
+Base.getindex(s::SymbolicSpinBasis, ::Colon) = [SpinSym(σ, s) for σ in [:x, :y, :z]]
 Base.hash(x::SymbolicSpinBasis, h::UInt) = hash(x.tags, hash(x.spin, hash(x.field, hash(x.label, h))))
 label(S::SymbolicSpinBasis) = S.label
 Base.getindex(s::SpinField, i) = SymbolicSpinBasis(i, s, s.spin, tags(s))
@@ -552,6 +553,9 @@ end
     Spo = Sone[:+]
     @test iszero(Sone[:+]^3)
     @test Sone[:z]^3 == Sone[:z]
+
+    # Test spin-1/2 identity: Sx^2 + Sy^2 + Sz^2 = 3/4 * I, and Colon operator
+    @test Shalf[:]' * Shalf[:] == 3 / 4 * Shalf[:I]
 
 end
 
