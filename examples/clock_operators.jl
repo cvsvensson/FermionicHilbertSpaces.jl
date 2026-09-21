68999aac3500c18b7a46c04c3839b7497a8fa527
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2026-09-21 12:02:28 +0200

    improve clock operators

--- examples/clock_operators.jl
+++ examples/clock_operators.jl
@@ -1,27 +1,19 @@
-# # Clock operators and symmetry-resolved entanglement
+# # Defining Clock operators 
 # 
-# This example defines a custom ``Z_D`` clock algebra and connects it to the
+# This example defines the ``Z_D`` clock operators and connects it to the
 # generic Hilbert-space and matrix-representation machinery. It then uses the
 # global clock charge to diagonalise a Hamiltonian sector by sector and to
 # resolve the entanglement entropy of a subsystem.
 
 using FermionicHilbertSpaces, LinearAlgebra, Plots, Arpack
-import FermionicHilbertSpaces:
-    AbstractBasisState,
-    AbstractSym,
-    GenericHilbertSpace,
-    apply_local_operator,
-    mat_eltype,
-    symbolic_group,
-    SectorConstraint
+import FermionicHilbertSpaces: AbstractBasisState, AbstractSym,
+    GenericHilbertSpace, apply_local_operator, mat_eltype,
+    symbolic_group, SectorConstraint
 import FermionicHilbertSpaces.NonCommutativeProducts: @nc, mul_effect
 
-# ## Defining the clock algebra
-# 
-# ``X`` shifts the local clock value and ``Z`` measures it. Their local
-# algebra is ``ZX = exp(2πim/D) XZ``. The `adjoint` flag lets the same type
-# represent both directions of the shift and both powers of the phase.
-struct ClockOp{T} <: AbstractSym # A clock operator of type T (:X or :Z).
+# ## Defining the clock operators and states
+# The clock operators are defined on a local Hilbert space of dimension ``D``, with basis states ``|n\rangle`` for ``n = 0, 1, ..., D-1``. The operator ``X`` shifts the local clock value, ``X |n\rangle = |n + 1\rangle``, and ``Z`` measures the local clock value: ``Z |n\rangle = \exp(2π i n / D) |n\rangle``.
+struct ClockOp{T} <: AbstractSym # A clock operator of type T (:X or :Z).   
     site::Int
     adjoint::Bool
 end
@@ -29,55 +21,54 @@ Base.adjoint(op::ClockOp{T}) where T = ClockOp{T}(op.site, !op.adjoint)
 X(i) = ClockOp{:X}(i, false)
 Z(i) = ClockOp{:Z}(i, false)
 @nc ClockOp
-## No symbolic simplification when multiplying clock operators.
+## We define mul_effect to return nothing, so that no symbolic simplification is performed when multiplying clock operators.
 mul_effect(::ClockOp, ::ClockOp) = nothing
 struct ClockState{D} <: AbstractBasisState # A clock state with dimension D.
     n::Int
     ClockState{D}(n) where D = new{D}(mod(n, D))
 end
 ## Action on local basis states.
-apply_local_operator(op::ClockOp{:X}, state::ClockState{D}, _, _) where D =
-    ClockState{D}(op.adjoint ? (state.n - 1) : state.n + 1), 1
-apply_local_operator(op::ClockOp{:Z}, state::ClockState{D}, _, _) where D =
-    state, exp((op.adjoint ? -1 : 1) * 2π * im * state.n / D)
+## The last two arguments, `space` and `precomp`, are required by the interface but not used here.
+## `space` can be used to dispatch on the type of hilbert space, while `precomp` can be used to pass precomputed data to the operator application for optimization purposes.
+function apply_local_operator(op::ClockOp{:X}, state::ClockState{D}, space, precomp) where D
+    newstate = ClockState{D}(state.n + (op.adjoint ? -1 : 1))
+    amp = 1
+    newstate, amp
+end
+function apply_local_operator(op::ClockOp{:Z}, state::ClockState{D}, space, precomp) where D
+    amp = exp((op.adjoint ? -1 : 1) * 2π * im * state.n / D)
+    state, amp
+end
 ## Clock operators have complex matrix elements.
 mat_eltype(::Type{<:ClockOp}) = ComplexF64
 ## Associate operators with Hilbert-space factors.
 symbolic_group(op::ClockOp) = op.site
 
 # This is enough to get most of the functionality of this package.
-# ## Hamiltonian and conservation laws
+# ## Using it in practice: Hamiltonian and conservation laws
 # Let's take the case D = 3 for 6 sites. We define a hamiltonian which conserves the particle number modulo D, and calculate the symmetry resolved entanglement entropy in the ground state.
 D = 3
 L = 6
-Hs = [GenericHilbertSpace(k, ClockState{D}.(0:(D-1))) for k in 1:L]
+states = [ClockState{D}(n) for n in 0:(D-1)]
+Hs = [GenericHilbertSpace(k, states) for k in 1:L]
 Hfull = tensor_product(Hs)
 clock_hamiltonian(J, h) = -J * sum(X(i)' * X(i + 1) + hc for i in 1:(L-1)) - h * sum(Z(i) + hc for i in 1:L)
 
-# Define a constraint and a function which divides the hilbert space up into sectors, and some functions to calculate the entropy and the ground state
+# Define a constraint and a function which divides the hilbert space up into sectors.
 function clock_sectors(H)
-    constraint = SectorConstraint(values -> mod(sum(values), D),
-        state -> state.n, factors(H))
+    constraint = SectorConstraint(
+        values -> mod(sum(values), D),  # sum the values mod D where
+        state -> state.n,               # the values are the local clock values
+        factors(H)                      # of each factor of the hilbert space
+    )
     return constrain_space(H, constraint)
 end
 Hzd = clock_sectors(Hfull)
 Hhalf = clock_sectors(tensor_product(Hs[1:div(L, 2)]))
 
-function sector_ground_state(q, J, h)
-    Hq = sector(q, Hzd)
-    hmat = Matrix(representation(clock_hamiltonian(J, h), Hq))
-    vals, vecs = eigs(Hermitian(hmat), nev=1, which=:SR)
-    return Hq, vals[1], vecs[:, 1]
-end
-
-##
-# Replace the final calculation/printing block with this.
-# S(A) = H(pₛ) + Σₛ pₛ Sₛ: charge-sharing entropy plus
-# entanglement remaining within a fixed subsystem charge sector.
-
+# Functions to calculate entropies.
 shannon(p; tol=1e-12) = -sum(x * log(x) for x in p if x > tol)
 von_neumann(ρ) = shannon(eigvals(Hermitian(ρ)))
-
 function symmetry_resolved_entropy(ρ, Hsub, D; tol=1e-12)
     p, Sq = zeros(D), zeros(D)
     for q in 0:(D-1)
@@ -90,34 +81,22 @@ function symmetry_resolved_entropy(ρ, Hsub, D; tol=1e-12)
     Swithin = dot(p, Sq)
     Stotal = von_neumann(ρ)
     @assert isapprox(Stotal, Hcharge + Swithin; atol=1e-6)
-    (; p, Sq, Hcharge, Swithin, Stotal)
+    (; Hcharge, Swithin, Stotal)
 end
-
-ptmap = partial_trace(sector(0, Hzd) => Hhalf)
+# We now calculate and plot the symmetry resolved entanglement entropy in the ground state of the hamiltonian for a range of values of ``h``
+Hgs = sector(0, Hzd)
 hs = range(0.05, 2.5; length=30)
+ptmap = partial_trace(Hgs => Hhalf)
 r = map(hs) do h
-    _, _, ψ0 = sector_ground_state(0, 1.0, h)
-    ρhalf = ptmap(ψ0)
+    hmat = representation(clock_hamiltonian(1.0, h), Hgs, :dense)
+    vals, vecs = eigs(Hermitian(hmat), nev=1, which=:SR)
+    ρhalf = ptmap(vecs[:, 1])
     symmetry_resolved_entropy(ρhalf, Hhalf, D)
 end
 
-S = getproperty.(r, :Stotal)
-Hq = getproperty.(r, :Hcharge)
-Sw = getproperty.(r, :Swithin)
-p = getproperty.(r, :p)
-Sq = getproperty.(r, :Sq)
-p1 = plot(hs, S, lw=3, c=:black, label="total  S(A)",
-    xlabel="h / J", ylabel="entropy",
-    title="Half-chain entanglement", yscale = :log10, legend = :bottomright)
-plot!(p1, hs, Hq, lw=2, c=:dodgerblue, label="charge entropy  H(pₛ)",)
-plot!(p1, hs, Sw, lw=2, c=:orangered, label="within-sector  ΣₛpₛSₛ")
-vline!(p1, [1], ls=:dash, c=:gray, label=false)
+p1 = plot(hs, getproperty.(r, :Stotal), lw=3, c=:black, label="total  S(A)",
+    xlabel="h / J", ylabel="entropy", size=(400, 250),
+    title="Half-chain entanglement", yscale=:log10, legend=:bottom)
+plot!(p1, hs, getproperty.(r, :Hcharge), lw=2, label="charge entropy  H(pₛ)",)
+plot!(p1, hs, getproperty.(r, :Swithin), lw=2, label="within-sector  ΣₛpₛSₛ")
 
-p2 = plot(hs, getindex.(p, 1), lw=3, c=:seagreen, label="p₀",
-    xlabel="h / J", ylabel="probability / entropy",
-    title="Resolved charge sectors")
-plot!(p2, hs, getindex.(p, 2), lw=3, c=:mediumpurple, label="p₁ = p₂")
-plot!(p2, hs, getindex.(Sq, 1), lw=2, ls=:dash, c=:seagreen, label="S₀")
-plot!(p2, hs, getindex.(Sq, 2), lw=2, ls=:dash, c=:mediumpurple, label="S₁ = S₂")
-vline!(p2, [1], ls=:dash, c=:gray, label=false)
-plot(p1, p2; layout=(1, 2), size=(800, 300))
