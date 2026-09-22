6d9639e84495566c766cc3c39105b64b9ebd17e0
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2026-09-22 13:53:05 +0200

    add AdditiveConstraint constructor

--- src/constraints.jl
+++ src/constraints.jl
@@ -130,10 +130,8 @@ struct AdditiveConstraint{T,H,F} <: AbstractConstraint
     spaces::H
     maps::F
 end
-AdditiveConstraint(allowed_values; spaces = missing, maps = missing) = AdditiveConstraint(allowed_values, spaces, maps)
-# AdditiveConstraint(allowed_values, functions) = AdditiveConstraint(allowed_values, missing, functions)
-# AdditiveConstraint(allowed_values, subspace::AbstractHilbertSpace, functions) = AdditiveConstraint(allowed_values, atomic_factors(subspace), functions)
-# AdditiveConstraint(allowed_values, subspace::AbstractGroupedHilbertSpace, functions) = AdditiveConstraint(allowed_values, atomic_factors(subspace), functions)
+AdditiveConstraint(allowed_values; spaces=missing, maps=missing) = AdditiveConstraint(allowed_values, spaces, maps)
+AdditiveConstraint(allowed_values, maps) = AdditiveConstraint(allowed_values; maps)
 supports_branch_pruning(::AdditiveConstraint) = true
 supports_filtering(::AdditiveConstraint{<:Any,Missing}) = false
 supports_filtering(::AdditiveConstraint) = true
