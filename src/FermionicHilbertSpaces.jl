37521e733c23505e9c3a0b8033c1afe3a8b96164
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2026-09-24 09:27:00 +0200

    add Base.last to TypedIterator

--- src/FermionicHilbertSpaces.jl
+++ src/FermionicHilbertSpaces.jl
@@ -81,6 +81,7 @@ Base.eltype(::Type{<:TypedIterator{T}}) where T = T
 Base.length(ti::TypedIterator) = length(ti.iter)
 Base.size(ti::TypedIterator) = size(ti.iter)
 Base.keys(ti::TypedIterator) = keys(ti.iter)
+Base.last(ti::TypedIterator{T}) where T = T(last(ti.iter))
 
 function _resolve_sector_permutations_and_weights end # for Combinatorics extension
 function cache_operator! end # for OhMyThreads extension
